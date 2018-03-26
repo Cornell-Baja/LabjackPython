4f8e9814fe11b2b468aefa20de4e63d5953a231e
sn438 <31802007+sn438@users.noreply.github.com>
2018-03-26 18:32:45 -0400

    Copied Shaur's Hall Effect Code to a New File Might Work Not Tested

--- Source/Examples/daqStreamHallEffect.py
+++ Source/Examples/daqStreamHallEffect.py
@@ -36,12 +36,13 @@ print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
     (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
 # Stream Configuration
-aScanListNames = ["AIN0", "AIN1", "CORE_TIMER","STREAM_DATA_CAPTURE_16","DIO1_EF_READ_A_AND_RESET","STREAM_DATA_CAPTURE_16","DIO0_EF_READ_A_AND_RESET","STREAM_DATA_CAPTURE_16","AIN2","AIN3"] #Scan list names to stream
+aScanListNames = ["CORE_TIMER","STREAM_DATA_CAPTURE_16", "DIO1_EF_READ_A_AND_RESET","STREAM_DATA_CAPTURE_16" ] #Scan list names to stream
 numAddresses = len(aScanListNames)
 aScanList = ljm.namesToAddresses(numAddresses, aScanListNames)[0]
 scanRate = 50
 scansPerRead = int(scanRate/2)
-clock_divisor = 64
+clock_divisor = 1
+
 
 try:
     # Configure the analog inputs' negative channel, range, settling time and
@@ -52,41 +53,24 @@ try:
     
                                              
 
-    aNames = ["AIN0_NEGATIVE_CH",
-                "AIN1_NEGATIVE_CH",
-                "DIO_EF_CLOCK0_ENABLE",
-                "DIO0_EF_ENABLE",
-                "DIO1_EF_ENABLE",
+    aNames = [
                 "DIO_EF_CLOCK0_ENABLE",
                 "DIO_EF_CLOCK0_DIVISOR",
-                "DIO0_EF_INDEX",
-                "DIO0_EF_ENABLE",
+                "DIO_EF_CLOCK0_ROLL_VALUE",
+                "DIO_EF_CLOCK0_ENABLE",
                 "DIO1_EF_INDEX",
                 "DIO1_EF_ENABLE",
-                "DIO_EF_CLOCK0_ENABLE",
-                "AIN2_NEGATIVE_CH",
-                "AIN3_NEGATIVE_CH",
-                # "STREAM_SETTLING_US",
-                 #"AIN0_RESOLUTION_INDEX",
-                #"AIN0_RANGE"
+                "DIO_EF_CLOCK0_ENABLE"
+
                 ]
-    aValues = [1,
-                ljm.constants.GND,
-                0,
-                0,
-                0,
-                0,
+    aValues = [ 0,
                 clock_divisor,
+                0,
+                1,
+                0,
                 3,
                 1,
-                3,
-                1,
-                1,
-                ljm.constants.GND,
-                ljm.constants.GND,
-                # 0,
-                # 0,
-                #.01
+                1
                 ]
 
     ljm.eWriteNames(handle, len(aNames), aNames, aValues)
@@ -100,7 +84,7 @@ try:
     totScans = 0
     totSkip = 0 # Total skipped samples
 
-    output_names=['torque', 'clock', 'rpm_1', 'rpm_2', 'dist_1','dist_2']
+    output_names=['clock','rpm_1']
     cur_log="dyno"+"_"+str(start.month)+"_"+str(start.day)+"_"+str(start.year)+"_"+str(start.hour)+"_"+str(start.minute)+"_"+str(start.second)+".csv"
 
     with open("test_dyno.csv", "w") as f:
@@ -118,39 +102,25 @@ try:
         data = ret[0]
         data_intermediate=np.reshape(data, (scansPerRead,len(aScanListNames)))
 
-        torque_high=data_intermediate[:,0]
-        torque_low=data_intermediate[:,1]
-        torque=torque_high-torque_low
-
-        a=data_intermediate[:,2]
-        b=data_intermediate[:,3]
-        clock=(a+b*65536)/(80000000/2)
-
-
-        a=data_intermediate[:,4]
-        b=data_intermediate[:,5]
-        rpm_1=a+b*65536
+        a = data_intermediate[:, 0]
+        b = data_intermediate[:, 1]
+        c = data_intermediate[:, 2]
+        d = data_intermediate[:, 3]
+        clock = (a + b * 65536) / (80000000 / 2)
+        rpm_1 = c + d * 65536
         if rpm_1 != 0:
-            rpm_1=(80000000/clock_divisor)/rpm_1
+            rpm_1 = (80000000 / clock_divisor) / rpm_1
 
 
-        a=data_intermediate[:,6]
-        b=data_intermediate[:,7]
-        rpm_2=a+b*65536
-        if rpm_2 !=0:
-            rpm_2=(80000000/clock_divisor)/rpm_2
+        output_data=np.array([clock, rpm_1]).transpose()
 
-        dist_1=data_intermediate[:,8]
-        dist_2=data_intermediate[:,9]
-
-
-
-        output_data=np.array([torque, clock, rpm_1, rpm_2, dist_1,dist_2]).transpose()
+        # print ("This is a: " + str(a))
+        # print ("This is b: " + str(b))
+        # print ("This is c: " + str(c))
+        # print ("This is d: " + str(d))
+        # print ("This is e: " + str(e))
 
         print (output_data)
-
-
-        
 
 
         f=open('test_dyno.csv','a')
